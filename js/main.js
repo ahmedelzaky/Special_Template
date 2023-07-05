6ebb17f18a20ecb0255283dbaabede1193a927ff
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-07-05 19:01:09 +0300

    V0.18

--- js/main.js
+++ js/main.js
@@ -10,23 +10,22 @@ let imgsArray = [
 ];
 
 // Function to set random image as background
-function setRandomImage() {
-  // try {
-  //   // Make a GET request to the Lorem Picsum API
-  //   //i am stopping the request because it use a lot of data
-  //   let response = await fetch("https://picsum.ph5otos/1920/1080");
+async function setRandomImage() {
+  // Set a random image from the picsum
 
-  //   // The response URL will contain a random image
-  //   let imageUrl = response.url;
-
-  //   landingPage.style.backgroundImage = `url(${imageUrl})`;
-  // } catch (error) {
-  //   console.error(error);
-
-  // Set a random image from the array
-  let randomNum = Math.floor(Math.random() * imgsArray.length);
-  landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
-  // }
+  try {
+    if (window.innerWidth > 768) {
+      let img = await fetch("https://picsum.photos/1920/1080");
+      landingPage.style.backgroundImage = `url(${img.url})`;
+    } else {
+      let img = await fetch("https://picsum.photos/1080/1920");
+      landingPage.style.backgroundImage = `url(${img.url})`;
+    }
+  } catch (error) {
+    //   Set a random image from the array
+    let randomNum = Math.floor(Math.random() * imgsArray.length);
+    landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
+  }
 }
 
 setRandomImage();
