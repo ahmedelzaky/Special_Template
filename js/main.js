2b06c85d6b9637448ffe740d707771ade867f49b
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-07-05 19:51:20 +0300

    V0.20

--- js/main.js
+++ js/main.js
@@ -13,22 +13,41 @@ let imgsArray = [
 async function setRandomImage() {
   // Set a random image from the picsum
 
-  try {
-    if (window.innerWidth > 768) {
-      let img = await fetch("https://picsum.photos/1920/1080");
-      landingPage.style.backgroundImage = `url(${img.url})`;
-    } else {
-      let img = await fetch("https://picsum.photos/1080/1920");
-      landingPage.style.backgroundImage = `url(${img.url})`;
-    }
-  } catch (error) {
-    //   Set a random image from the array
-    let randomNum = Math.floor(Math.random() * imgsArray.length);
-    landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
-  }
+  // try {
+  //   let img;
+  //   if (window.innerWidth > 768) {
+  //     img = await fetch("https://picsum.photos/1920/1080");
+  //     landingPage.style.backgroundImage = `url(${img.url})`;
+  //   } else {
+  //     img = await fetch("https://picsum.photos/1080/1920");
+  //     landingPage.style.backgroundImage = `url(${img.url})`;
+  //   }
+  //   if (!img.ok) {
+  //   Set a random image from the array
+  let randomNum = Math.floor(Math.random() * imgsArray.length);
+  landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  // }
 }
 
 setRandomImage();
 
 // Change background image url
 setInterval(setRandomImage, 10000);
+
+let settingsBoxg = document.querySelector(".settings-box");
+let gear = document.querySelector(".gear");
+let isOpne = false;
+
+gear.addEventListener("click", (e) => {
+  if (!isOpne) {
+    settingsBoxg.style.left = 0;
+    gear.style.left = "200px";
+  } else {
+    settingsBoxg.style.left = "-251px";
+    gear.style.left = 0;
+  }
+  isOpne = !isOpne;
+});
