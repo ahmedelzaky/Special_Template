9ad89a3da7d7961f0bd269b04452c912e4a18b60
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-07-08 17:38:36 +0300

    V0.35

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ let isOpne = false;
 gearBox.addEventListener("click", (e) => {
   if (!isOpne) {
     settingsBox.style.left = 0;
-    gearBox.style.left = "200px";
+    gearBox.style.left = "240px";
   } else {
     settingsBox.style.left = "-251px";
     gearBox.style.left = 0;
@@ -52,9 +52,40 @@ colorsList.forEach((li) => {
   });
 });
 
-//landing page
+// random background option
 
+const buttons = document.querySelectorAll(".settings-box .option-box button");
+let stat = localStorage.getItem("background-statue");
 const landingPage = document.querySelector(".landing-page");
+
+// check stat in local storge value
+if (stat != null) {
+  landingPage.style.backgroundImage = localStorage.getItem("background-url");
+
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+
+    if (stat == "true" && btn.classList.contains("yes")) {
+      btn.classList.add("active");
+    }
+    if (stat == "false" && btn.classList.contains("no")) {
+      btn.classList.add("active");
+    }
+  });
+}
+// add Event Listener for background options
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    stat = e.target.classList.contains("yes");
+    localStorage.setItem("background-statue", stat);
+  });
+});
+
+//landing page
 
 // Get array of images
 let imgsArray = [
@@ -65,30 +96,36 @@ let imgsArray = [
   "./images/slide05.jpg",
 ];
 
+// Varible to control interval
+
 // Function to set random image as background
 async function setRandomImage() {
-  // Set a random image from the picsum
+  if (stat == "true" || stat == true) {
+    // Set a random image from the picsum
 
-  // try {
-  //   let img;
-  //   if (window.innerWidth > 768) {
-  //     img = await fetch("https://picsum.photos/1920/1080");
-  //     landingPage.style.backgroundImage = `url(${img.url})`;
-  //   } else {
-  //     img = await fetch("https://picsum.photos/1080/1920");
-  //     landingPage.style.backgroundImage = `url(${img.url})`;
-  //   }
-  //   if (!img.ok) {
-  //   Set a random image from the array
-  let randomNum = Math.floor(Math.random() * imgsArray.length);
-  landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    // try {
+    //   let img;
+    //   if (window.innerWidth > 768) {
+    //     img = await fetch("https://picsum.photos/1920/1080");
+    //     landingPage.style.backgroundImage = `url(${img.url})`;
+    //   } else {
+    //     img = await fetch("https://picsum.photos/1080/1920");
+    //     landingPage.style.backgroundImage = `url(${img.url})`;
+    //   }
+    //   localStorage.setItem("background-url", `url(${img.url})`);
+    //   if (!img.ok) {
+        //   Set a random image from the array
+        let randomNum = Math.floor(Math.random() * imgsArray.length);
+        landingPage.style.backgroundImage = `url(${imgsArray[randomNum]})`;
+        localStorage.setItem("background-url", `url(${imgsArray[randomNum]})`);
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
 }
 
 setRandomImage();
 
 // Change background image url
-setInterval(setRandomImage, 10000);
+setInterval(setRandomImage, 5000);
